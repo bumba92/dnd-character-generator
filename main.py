344e1d708e36d52f5d97d394703bbc9c4ba67d60
bumba <tim.krainer@googlemail,com>
2026-06-27 20:47:22 +0200

    Definition von class

--- main.py
+++ main.py
@@ -11,9 +11,32 @@ def print_character(character):
     print(f"Klasse: {character['class']}")
     print(f"Level: {character['level']}")
 
-name = input("Charaktername: ")
-character_class = input("Charakterklasse: ")
-level = int(input("Level: "))
+class Character:
+    def __init__(self, name, race, character_class, level, background):
+        self.name = name
+        self.race = race
+        self.character_class = character_class
+        self.level = level
+        self.background = background
 
-character = create_character(name, character_class, level)
-print_character(character)
+    def describe(self):
+        print(
+            f"Name: {self.name}\n"
+            f"Rasse: {self.race}\n"
+            f"Klasse: {self.character_class}\n"
+            f"Level: {self.level}\n"
+            f"Hintergrund: {self.background}"
+        )
+
+    def get_PB(self):
+        return (self.level - 1) // 4 + 2
+    
+hero_1 = Character("Aragorn", "Mensch", "Krieger", 5, "König von Gondor")
+hero_2 = Character("Gandalf", "Maia", "Zauberer", 20, "Weiser Mentor")
+
+hero_1.describe()
+print (f"Proficiency Bonus: {hero_1.get_PB()}")
+print("\n")
+hero_2.describe()
+print (f"Proficiency Bonus: {hero_2.get_PB()}")
+
