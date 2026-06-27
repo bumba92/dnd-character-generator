84a8441bb512aae4448f844fa29b84d988dc377a
bumba <tim.krainer@googlemail,com>
2026-06-27 16:16:58 +0200

    Basics: Variables, dicts, lists

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("D&D Character Sheet Generator")
+character = {
+    "name": "Thorin",
+    "class": "Fighter",
+    "Level": 5,
+    "race": "Dwarf",
+    "languages": ["Common", "Dwarvish"],
+    "equipment": ["Battleaxe", "Shield", "Chainmail"],
+}
+
+#Werte abrufen:
+
+print(character["languages"])  # Ausgabe: ['Common', 'Dwarvish']
+
+print(character["equipment"][0])
+
+for item in character["equipment"]:
+    print(f"-{item}")
